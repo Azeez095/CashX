acfd2ba875acae6e3cc82fbdb10635a2e8cbabbb
AMOO AKOLADE <106633926+Azeez095@users.noreply.github.com>
2024-12-24 15:40:08 +0100

    updated

--- src/views/dashboard/Insight.vue
+++ src/views/dashboard/Insight.vue
@@ -7,7 +7,7 @@
         </div>
         <div class="grid grid-cols-1 lg:grid-cols-2 gap-x-6 gap-y-10 text-center">
             <div class="bg-[#141414] min-h-[200px] grid grid-cols-1 justify-center items-center rounded-[10px]">
-                <div class="px-4 flex lg:justify-center sm:text-2xl lg:text-4xl">
+                <div class="px-4 flex lg:justify-center sm:text-2xl lg:text-3xl">
                     <div class="flex flex-col gap-2 text-white w-full">
                         Total Budget Amount
                         <h3 class=" text-white font-bold">#500, 000</h3>
@@ -31,7 +31,7 @@
             </div>
             <div class="bg-[#141414] min-h-[200px] grid grid-cols-1 justify-center items-center rounded-[10px]">
                 <div class="px-4 flex lg:justify-centerr">
-                    <div class="flex flex-col gap-4 text-white w-full text-4xl font-bold">
+                    <div class="flex flex-col gap-4 text-white w-full sm:text-2xl lg:text-3xl font-bold">
                         Total Balance
                         <h1 class="flex flex-col gap-2 text-white">#40, 000</h1>
                     </div>
